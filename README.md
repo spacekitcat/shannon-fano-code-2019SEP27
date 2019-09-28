feaa354a8954d229351eb9da91f604bcac0f8cc4
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-09-28 16:36:12 +0100

    feat(done): Now generates the Shannon-Fano codings for the test files, added the example from Wikipedia and updated the README.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Small project to explore the Shannon-Fano coding scheme (just for fun).
 
 ## Progress
 
-I've implemented a Transform stream to count byte occurrences from an input stream.
+I've implemented a Transform stream to count byte occurrences from an input stream, generate the tree and then traverse it to get generate the codes.
 
 ## Build
 
@@ -46,26 +46,82 @@ File contents: `HELLOWORLD` (outputs are UTF-8 representation).
 
 ```bash
 $ node . resources/a.txt
-Not implemented yet.
+000	68
+0010	69
+0011	72
+01	76
+10	79
+110	82
+111	87
 ```
 
 File contents: `The quick brown fox jumps over the lazy dog.` (outputs are UTF-8 representation).
 
 ```bash
 $ node . resources/b.txt
-Not implemented yet.
+000	32
+0010	46
+00110	84
+00111	97
+01000	98
+010010	99
+010011	100
+0101	101
+01100	102
+01101	103
+01110	104
+01111	105
+10000	106
+10001	107
+10010	108
+100110	109
+100111	110
+1010	111
+10110	112
+10111	113
+11000	114
+11001	115
+11010	116
+11011	117
+11100	118
+11101	119
+11110	120
+111110	121
+111111	122
 ```
 
 File contents: `$$$$$zzzzzzzzzzzzzzzzzzzzzzz*********G@@@` (outputs are UTF-8 representation).
 
 ```bash
 $ node . resources/c.txt
-Not implemented yet.
+00    36
+010   42
+0110  64
+0111  71
+1	  122
 ```
 
-File contents: `A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED` (outputs are UTF-8 representation). This example is from Wikipedia article on Huffman coding and it matches.
+File contents: `A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED` (outputs are UTF-8 representation). This example is from Wikipedia article on `Huffman_coding` and it matches.
 
 ```bash
 $ node . resources/d.txt
-Not implemented yet.
+00	65
+010	66
+011	67
+10	68
+110	69
+111	95
 ```
+
+File contents: `AAAAAAAAAAAAAAABBBBBBBCCCCCCDDDDDDEEEEE` (outputs are UTF-8 representation). This example is from Wikipedia article on `Shannon-Fano_coding` and it matches.
+
+```bash
+$ node . resources/e.txt
+00	65
+01	66
+10	67
+110	68
+111	69
+```
+
+
--- src/main.js
+++ src/main.js
@@ -2,9 +2,48 @@ import fs from 'fs';
 import process from 'process';
 import ByteOccurrenceCountStream from './ByteOccurrenceCountStream';
 import Node from './Node';
+import divideNodeList from './divideNodeList';
 
-const createLeafList = occurrences =>
-  Object.keys(occurrences).map(key => new Node(occurrences[key], key));
+const createLeafList = occurrences => {
+  const filteredList = Object.keys(occurrences).filter(key => key !== '10');
+
+  return filteredList.map(key => {
+    if (key === '10') {
+      return null;
+    }
+    return new Node(occurrences[key], key);
+  });
+};
+
+const generateTree = leafList => {
+  if (leafList.length === 1) {
+    return new Node(leafList[0].getValue(), leafList[0].getKey());
+  }
+
+  const dividedList = divideNodeList(leafList);
+  const internalNode = new Node('', '');
+  internalNode.leftChild = generateTree(dividedList.left);
+  internalNode.rightChild = generateTree(dividedList.right);
+  return internalNode;
+};
+
+const traverseTree = (rootNode, prefix = '') => {
+  if (rootNode === null) {
+    return;
+  }
+
+  if (rootNode.getLeftChild() === null && rootNode.getRightChild() === null) {
+    process.stdout.write(`${prefix}\t${rootNode.getKey()}\n`);
+  }
+
+  if (rootNode.getLeftChild() !== null) {
+    traverseTree(rootNode.getLeftChild(), `${prefix}0`);
+  }
+
+  if (rootNode.getRightChild() !== null) {
+    traverseTree(rootNode.getRightChild(), `${prefix}1`);
+  }
+};
 
 /** This is prototype code, hence the lack of tests. */
 const SayHello = () => {
@@ -13,7 +52,9 @@ const SayHello = () => {
   const occurrenceCountStream = new ByteOccurrenceCountStream();
 
   fileReadStream.on('end', () => {
-    console.log(createLeafList(occurrenceCountStream.getOccurrences()));
+    const leafList = createLeafList(occurrenceCountStream.getOccurrences());
+    const treeRootNode = generateTree(leafList);
+    traverseTree(treeRootNode);
   });
 
   fileReadStream.pipe(occurrenceCountStream);
